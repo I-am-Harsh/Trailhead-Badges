8ac1a5e62abc9d540c75f9b6d431994c469aedad
Harsh Verma <verma.harsh98@gmail.com>
2020-02-24 15:36:40 +0530

    Updates

---
+++ precache-manifest.f8f5e292540eeaaba0361b99972b66ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ed2f32383ff6602a07459b2321f868",
+    "url": "/Trailhead-Badges/index.html"
+  },
+  {
+    "revision": "d100037ad0b966b3427f",
+    "url": "/Trailhead-Badges/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "55e609086d654056866a",
+    "url": "/Trailhead-Badges/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d100037ad0b966b3427f",
+    "url": "/Trailhead-Badges/static/js/2.4559264a.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/Trailhead-Badges/static/js/2.4559264a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e609086d654056866a",
+    "url": "/Trailhead-Badges/static/js/main.c0cbbf56.chunk.js"
+  },
+  {
+    "revision": "29cb50ccafaee87676ae",
+    "url": "/Trailhead-Badges/static/js/runtime-main.0a3c2643.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Trailhead-Badges/static/media/logo.5d5d9eef.svg"
+  }
+]);
